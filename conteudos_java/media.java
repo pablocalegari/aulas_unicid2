58c3b450764f3d40e7b952d239b150e058b77c25
pablocalegari <pabloantoniocalegari@gmail.com>
2026-02-24 20:58:44 -0300

    Create media.java

---
+++ conteudos_java/media.java
@@ -0,0 +1,15 @@
+package aula2402;
+
+public class Media {
+
+	public static void main(String[] args) {
+	
+		double v1 = 10.45;
+		double v2 = 24.78;
+		
+		double media = (v1+v2)/2;
+				
+				System.out.println("A média é: "+ media);
+	}
+
+}
