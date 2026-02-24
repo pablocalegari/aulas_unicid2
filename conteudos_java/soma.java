f23f5d75a8d290d111423ce0e2ac55effcecb827
pablocalegari <pabloantoniocalegari@gmail.com>
2026-02-24 20:58:20 -0300

    Create soma.java

---
+++ conteudos_java/soma.java
@@ -0,0 +1,12 @@
+package aula2402;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 30;
+		int soma = (a+b);
+		
+		System.out.println("Reultado "+ soma);
+	}
+}
