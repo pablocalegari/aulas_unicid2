915ae190ffcd4506e5fb4f2cbd0b39e378755ed9
pablocalegari <pabloantoniocalegari@gmail.com>
2026-02-24 21:11:18 -0300

    Create App2.java

---
+++ conteudos_java/App2.java
@@ -0,0 +1,13 @@
+package aula2402;
+
+public class App2 {
+public static void main(String[] args) {
+	int i;
+	int j = 0;
+	
+	for (i = 1; i <101; i++){
+		 j +=i;
+	}
+	System.out.println(j);	
+}
+}
