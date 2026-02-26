d608b13c2999e2eae709ec618742b45fc48bb694
pablocalegari <pabloantoniocalegari@gmail.com>
2026-02-26 13:46:10 -0300

    Create treino2.java

---
+++ conteudos_java/treino2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      mostrarMeuNome("Pablo" , 19);
+      
+    }
+    
+    
+    private static void mostrarMeuNome (String nome , int idade){
+      System.out.println("Meu nome é " + nome + " e minha idade é: " + idade  );
+    }
+}
