9bf4b790dadad7e5eedd2b4213243eca0a43f964
pablocalegari <pabloantoniocalegari@gmail.com>
2026-02-26 12:58:42 -0300

    Create treinos.java

---
+++ conteudos_java/treinos.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("TreininhoJava"); //LN usado para quebrar linha
+      
+      //Tipos Primitivos
+      
+      int idade = 32; //aloca 32 bits
+      double altura = 1.33; //16 bits
+      char letra = 'a'; // 16 bits
+      Long numerosGrandes = 14564356436L; //64bits
+      
+      //Tipos não primitivos
+      
+      String nome = "Pablo Calegari";
+     String nomeUpperCase = nome.toUpperCase(); //exemplo de alocação de variaveis 
+      System.out.print(nomeUpperCase); //print
+      
+    }
+}
